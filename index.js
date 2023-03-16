5c88bcf859b3ed4a18705de098d9ee3940589e0c
RicardoUy <rvuy62@yahoo.com>
2023-03-15 19:39:07 -0500

    Second submit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+welcomeMessage;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+excitedWelcomeMessage;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+shortGreeting;
